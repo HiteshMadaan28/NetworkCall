ab0d78cf18cdf0634fb8a634dae90ad8f9eb9c10
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-06-18 08:18:08 +0530

    Initial Commit

---
+++ NetworkCall/NetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallApp.swift
+//  NetworkCall
+//
+//  Created by Hitesh Madaan on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
